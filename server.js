9377886ef55eedc2db8898aa12164585a77e0789
Aravind Parappil <aravindparappil@gmail.com>
2019-12-04 17:37:21 -0500

    Removed unwanted sql connection

--- server.js
+++ server.js
@@ -2,28 +2,10 @@
 
 const express = require('express'),
 app = express(),
-port = process.env.PORT || 3000;
+port = 52112 //process.env.PORT || 3000;
 bodyParser = require('body-parser');
-var https = require('https');
-var fs = require('fs');
-
-const mysql = require('mysql');
-const mc = mysql.createConnection({
-    host: 'remotemysql.com',
-    port: '3306',
-    user: 'S16nPYllpL',
-    password: 'PQ1OrgRPc5',
-    database: 'S16nPYllpL'
-});
-mc.connect();
-
-setInterval(function () {
-	console.log("Making a query!!! Server.js");
-    mc.query('SELECT 1');
-}, 300000);
 
 const cors = require('cors')
-
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -34,13 +16,6 @@ app.use(cors({credentials: true, origin: true}))
 var routes = require('./app/routes/routes'); //importing route
 routes(app); //register the route
 
-//app.listen(port);
-//console.log("[BoardGames] NodeJS Server started on: " + port);
+app.listen(port);
+console.log("[BoardGames] NodeJS Server started on: " + port);
 
-https.createServer({
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
-}, app)
-.listen(port, function () {
-  console.log("[BoardGames] HTTPS NodeJS Server started on: " + port);
-});
